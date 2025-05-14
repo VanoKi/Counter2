8679dd30fac3ab1beba961ee70ac3a67a1a040de
VanoKi <kirsvano@gmail.com>
2025-05-14 15:08:33 +0300

    feat: counter

--- counter2/src/components/Button.tsx
+++ counter2/src/components/Button.tsx
@@ -3,16 +3,18 @@ import * as React from 'react';
 
 type Props = {
   title: string
-  callBack: () => void
-
+  callBack?: () => void
+  disabled?: boolean
 };
 export const Button = (props: Props) => {
-  const {title, callBack} = props
+  const {title, callBack, disabled} = props
   return (
     <button
-      title={title}
+      className={'btn'}
       onClick={callBack}
+      disabled={disabled}
     >
+      {title}
     </button>
   );
 };
--- counter2/src/components/CounterDisplay.tsx
+++ counter2/src/components/CounterDisplay.tsx
@@ -1,18 +1,30 @@
 // @flow 
 import * as React from 'react';
 import {Button} from "./Button.tsx";
+import {useState} from "react";
 
 type Props = {
   
 };
 export const CounterDisplay = (props: Props) => {
+  let minVal = 0
+  let maxVal = 5
+  let [val, setVal] = useState(minVal)
+
+  const incVal = (minVal:number, maxVal:number) => {
+    if (val < maxVal) {
+      val = val + 1
+      setVal(val)
+    }
+  }
+
   return (
     <div className={'counter-display'}>
       <div className={'display'} >
-        <span></span>
+        <span>{val}</span>
       </div>
       <div className={'btn-wrap'}>
-        <Button title={'increment'}/>
+        <Button title={'increment'} callBack={() => incVal(minVal, maxVal)}/>
         <Button title={'reset'}/>
       </div>
     </div>
