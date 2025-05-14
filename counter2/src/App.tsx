afe49502a8a46f518ac4b3981d96302ea8fd49dd
VanoKi <kirsvano@gmail.com>
2025-05-14 17:31:20 +0300

    feat: add showVal to tinput

--- counter2/src/App.tsx
+++ counter2/src/App.tsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-// import {SetDisplay} from "./components/setDisplay.tsx";
+import {SetDisplay} from "./components/setDisplay.tsx";
 import {CounterDisplay} from "./components/CounterDisplay.tsx";
 
 function App() {
   const [minVal, setMinVal] = useState(0)
-  const [maxVal, setMaxVal] = useState(5)
+  const [maxVal, setMaxVal] = useState(3)
   // const [val, setVal] = useState(0)
 
   return (
     <div className={'app'}>
-      {/*<SetDisplay minVal={minVal} maxVal={maxVal} setMinVal={() => setMinVal(minVal)}/>*/}
+      <SetDisplay
+        minVal={minVal}
+        maxVal={maxVal}
+        setMinVal={setMinVal}
+        setMaxVal={setMaxVal}
+      />
       <CounterDisplay
         minVal={minVal}
         maxVal={maxVal}
--- counter2/src/components/setDisplay.tsx
+++ counter2/src/components/setDisplay.tsx
@@ -3,15 +3,25 @@ import {Button} from "./Button.tsx";
 type Props = {
   minVal: number
   maxVal: number
+  setMinVal: (val:number) => void
+  setMaxVal: (val:number) => void
 };
 
 export const SetDisplay = (props: Props) => {
-  const [minVal, maxVal] = props
+  const {minVal, maxVal, setMinVal, setMaxVal} = props
+  const showValue = (val:string) => console.log(val)
   return (
     <>
       <div className={''}>
-        <input type={"number"} value={minVal}/>
-        <input type={"number"} value={maxVal}/>
+        <input
+          type={"number"}
+          value={minVal}
+          onChange={(e) => showValue(e.target.value)}
+        />
+        <input
+          type={"number"}
+          value={maxVal}
+        />
       </div>
       <div className={'btn-wrap'}>
         <Button title={'Set'}/>
