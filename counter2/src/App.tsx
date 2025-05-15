8673fde8a540d5b6751905942fd9d58be432b800
VanoKi <kirsvano@gmail.com>
2025-05-15 17:52:53 +0300

    feat: add counter

--- counter2/src/App.tsx
+++ counter2/src/App.tsx
@@ -4,30 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let minVal = 0
+  let maxVal = 3
+  let [count, setCount] = useState(minVal)
+  
+  const incVal = () => {
+    if (count < maxVal) {
+      setCount(count + 1)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={'counter'}>
+        <div>
+          <span>{count}</span>
+        </div>
+        <div>
+          <button onClick={incVal}>increment</button>
+          <button>reset</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
