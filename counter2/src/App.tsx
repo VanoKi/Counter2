50343231b3071e7c633d4099e1f200bd7ee165df
VanoKi <kirsvano@gmail.com>
2025-05-15 18:21:26 +0300

    feat: add variables

--- counter2/src/App.tsx
+++ counter2/src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import {type ChangeEvent, use, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let minVal = 0
-  let maxVal = 3
+  let min = 0
+  let max = 3
+
+  let [minVal, setMinVal] = useState(min)
+  let [maxVal, setMaxVal] = useState(max)
   let [count, setCount] = useState(minVal)
   console.log(`count is: `, count)
   
@@ -14,16 +17,30 @@ function App() {
       setCount(count + 1)
     }
   }
-
   const resetVal = () => {
     setCount(minVal)
   }
-
   const isIncDisabled = count === maxVal
   const isResetDisabled = count === minVal
 
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.currentTarget.value)
+
+  }
+
   return (
     <>
+      <div className={'input-panel'}>
+        <div>
+          <label htmlFor={'minVal'}>minVal</label>
+          <input id={'minVal'} value={minVal} type={'number'} onChange={onChangeHandler}></input>
+        </div>
+        <div>
+          <label htmlFor={'maxVal'}>minVal</label>
+          <input id={'maxVal'} value={maxVal} type={'number'} onChange={onChangeHandler}></input>
+        </div>
+        <button>set</button>
+      </div>
       <div className={'counter'}>
         <div>
           <span className={isIncDisabled ? 'red' : ''} >{count}</span>
